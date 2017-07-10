e0205b05bf91ac8cba790740622a9c2fece51807
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2017-07-10 19:40:16 +0300

    Invalid namespace exception

---
+++ src/Exceptions/InvalidNamespaceException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GeniusTS\TranslationManager\Exceptions;
+
+
+/**
+ * Class InvalidNamespaceException
+ *
+ * @package GeniusTS\TranslationManager
+ */
+class InvalidNamespaceException extends \Exception
+{
+
+}
